04b782f7f86cb1cf75c3a27d2654c75d5c813234
Simon Kaminski <szy.kaminski@gmail.com>
2022-10-11 00:55:52 +0200

    added db context with proper connection string

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,14 @@
+using backend;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Configure
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
