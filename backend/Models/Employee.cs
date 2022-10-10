bc600a2254b5906ef4bb0bff1945cbd2a7fa6655
Simon Kaminski <szy.kaminski@gmail.com>
2022-10-11 00:13:08 +0200

    created employee model and context

---
+++ backend/Models/Employee.cs
@@ -0,0 +1,10 @@
+namespace backend.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public int Index { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ backend/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options): base (options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
